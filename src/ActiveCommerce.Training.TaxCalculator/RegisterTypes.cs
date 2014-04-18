51ccdb85ab4b234788f4b32224af9ed57e978691
Nick Wesselman <nw@activecommerce.com>
2014-04-18 14:56:43 -0500

    added custom tax calculator example

---
+++ src/ActiveCommerce.Training.TaxCalculator/RegisterTypes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Practices.Unity;
+
+namespace ActiveCommerce.Training.TaxCalculator
+{
+    public class RegisterTypes : ActiveCommerce.IoC.ITypeRegistration
+    {
+        public void Process(Microsoft.Practices.Unity.IUnityContainer container)
+        {
+            container.RegisterType<ActiveCommerce.Taxes.ITaxCalculator, TrainingTaxCalculator>("training");
+        }
+
+        public int SortOrder
+        {
+            get { return 0; }
+        }
+    }
+}
---
+++ src/ActiveCommerce.Training.TaxCalculator/TrainingTaxCalculator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActiveCommerce.Training.TaxCalculator
+{
+    public class TrainingTaxCalculator : ActiveCommerce.Taxes.TaxCalculatorBase<Taxes.TaxConfiguration>
+    {
+        public readonly decimal RATE = .055m;
+        public readonly string TYPE = "DEFAULT";
+        public readonly string SHIPPING = "SHIPPING";
+        public readonly string HANDLING = "HANDLING";
+
+        public override Taxes.TaxTotals GetTaxes(IEnumerable<Taxes.TaxInquiry> order)
+        {
+            var taxTotals = new Taxes.TaxTotals();
+            var productTaxes = new List<ActiveCommerce.Taxes.TaxLine>();
+            taxTotals.ProductTax = productTaxes;
+            foreach (var taxLine in order)
+            {
+                if (taxLine.IsShipping)
+                {
+                    taxTotals.ShippingTax = GetTaxLine(SHIPPING, taxLine.Total);
+                }
+                else if (taxLine.IsHandling)
+                {
+                    taxTotals.HandlingTax = GetTaxLine(HANDLING, taxLine.Total);
+                }
+                else
+                {
+                    var productLine = GetTaxLine(taxLine.ProductCode, taxLine.Total);
+                    productTaxes.Add(productLine);
+                }
+            }
+            return taxTotals;
+        }
+
+        public Taxes.TaxLine GetTaxLine(string productCode, decimal total)
+        {
+            return new Taxes.TaxLine
+                    {
+                        TaxedAmount = total,
+                        ProductCode = productCode,
+                        Jurisdictions = new List<Taxes.TaxJurisdiction>
+                        {
+                            /* just return a single jurisdiction for this example.
+                             * if you need AC to track tax breakdown by jurisdiction,
+                             * you would need to add a line for each jurisdiction according to its
+                             * tax rate. */
+                            new Taxes.TaxJurisdiction {
+                                 Name = TYPE,
+                                 Type = TYPE,
+                                 Rate = RATE,
+                                 Tax = total * RATE
+                            }
+                        }
+                    };
+        }
+
+        public override bool WillHandle()
+        {
+            return true;
+        }
+    }
+}
