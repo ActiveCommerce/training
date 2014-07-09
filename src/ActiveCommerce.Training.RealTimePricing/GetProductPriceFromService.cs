95b89e98ee846dfb96374dcc12144f5c023e33c6
Nick Wesselman <nw@activecommerce.com>
2014-07-08 20:06:15 -0500

    added caching to real-time pricing example

--- src/ActiveCommerce.Training.RealTimePricing/GetProductPriceFromService.cs
+++ src/ActiveCommerce.Training.RealTimePricing/GetProductPriceFromService.cs
@@ -1,14 +1,31 @@
 ﻿using ActiveCommerce.Prices.Pipelines.GetProductTotals;
+using Sitecore.Caching;
 
 namespace ActiveCommerce.Training.RealTimePricing
 {
     public class GetProductPriceFromService : IGetProductTotalsProcessor
     {
+        private static Cache _priceCache;
+
+        static GetProductPriceFromService()
+        {
+            _priceCache = Cache.GetNamedInstance("acPrices", 1 * 1024 * 1024);
+        }
+
         public void Process(GetProductTotalsArgs args)
         {
             var product = args.Product;
-            var service = new Services.ProductServiceClient();
-            var price = service.GetPrice(product.Code);
+            decimal price = 0;
+            if (_priceCache.ContainsKey(product.Code))
+            {
+                price = (System.Decimal)_priceCache[product.Code];
+            }
+            else
+            {
+                var service = new Services.ProductServiceClient();
+                price = service.GetPrice(product.Code);
+                _priceCache.Add(product.Code, price, 8, System.DateTime.UtcNow.AddMinutes(1));
+            }
             args.Totals.PriceExVat = price;
             args.HasPrice = true;
         }
