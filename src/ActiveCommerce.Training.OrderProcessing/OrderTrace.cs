1794e48db2409b6b73589913a0f45836e1c09765
Nick Wesselman <nwesselman@hansondodge.com>
2014-07-24 18:07:52 -0500

    added example of tracing order steps for performance debugging

---
+++ src/ActiveCommerce.Training.OrderProcessing/OrderTrace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ActiveCommerce.OrderProcessing;
+
+namespace ActiveCommerce.Training.OrderProcessing
+{
+    public class OrderTrace : IOrderPipelineProcessor
+    {
+        public string Message { get; set; }
+
+        public void Process(OrderPipelineArgs args)
+        {
+            Sitecore.Diagnostics.Log.Info(string.Format("{0} for Customer {1}", Message, args.Cart.CustomerInfo.CustomerId), this);
+        }
+    }
+}
