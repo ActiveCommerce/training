4cad2513638a1f6a958a480f956570ae439ad662
ddysart <derek@djdysart.com>
2014-11-04 21:24:51 -0600

    Fix error in url calculation, also account for variants changing the URL

--- src/ActiveCommerce.Training.ProductQuantity/skins/training/scripts/product.js
+++ src/ActiveCommerce.Training.ProductQuantity/skins/training/scripts/product.js
@@ -10,10 +10,20 @@
             }
             var link = ActiveCommerce.Product.Variants.$elements.addToCart;
             var url = link.attr('rel').split("/");
-            url = url.splice(5, url.length - 4);
+            url.splice(5, url.length - 4);
             url.push(val);
             link.attr('rel', url.join("/"));
         });
     };
 
 })(ActiveCommerce.Product.init);
+
+
+ActiveCommerce.Product.Variants.updateDisplay = (function (baseUpdateDisplay) {
+
+    return function () {
+        baseUpdateDisplay();
+        // Force a call to quatity change to ensure it stays in the rel attribute
+        $('#options-qty').change();
+    };
+})(ActiveCommerce.Product.Variants.updateDisplay);
