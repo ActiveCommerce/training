3e452b3a3cb5fdb4bcca89be28f4bf12d44d1d3b
Nick Wesselman <nw@activecommerce.com>
2015-06-17 16:51:41 -0500

    updated tax calculator example to show how to access shipping address and other properties

--- src/ActiveCommerce.Training.TaxCalculator/TrainingTaxCalculator.cs
+++ src/ActiveCommerce.Training.TaxCalculator/TrainingTaxCalculator.cs
@@ -14,6 +14,28 @@ namespace ActiveCommerce.Training.TaxCalculator
 
         public override Taxes.TaxTotals GetTaxes(IEnumerable<Taxes.TaxInquiry> order)
         {
+            /**
+             * Address and other information is available in properties of the base class.
+             */
+
+            //Shipping address is usually the relevant one for tax calculation
+            var shippingAddress = this.Shipping;
+            var city = shippingAddress.City;
+            var state = shippingAddress.State;
+            var countryCode = shippingAddress.Country.Code;
+            var postalCode = shippingAddress.Zip;
+
+            //Billing address is not usually relevant for taxes, but available
+            var billingAddress = this.Billing;
+
+            //Config values from the Tax Calculator item are also available.
+            //You can extend the TaxConfiguration class and map to additional template fields if needed.
+            var config = this.TaxConfiguration;
+
+            /**
+             * You should return tax amount for each order line, including
+             * shipping and handling.
+             */ 
             var taxTotals = new Taxes.TaxTotals();
             var productTaxes = new List<ActiveCommerce.Taxes.TaxLine>();
             taxTotals.ProductTax = productTaxes;
@@ -58,6 +80,11 @@ namespace ActiveCommerce.Training.TaxCalculator
                     };
         }
 
+        /// <summary>
+        /// Your tax calculator can decide whether it can handle taxes for the order,
+        /// based on the shipping address and tax configuration.
+        /// </summary>
+        /// <returns></returns>
         public override bool WillHandle()
         {
             return true;
